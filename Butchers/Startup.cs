34465041fc9d883879da0b43bc69dde4ebe5a6ce
dangreenacres <dangreenacres@gmail.com>
2018-02-05 14:34:47 +0000

    Add project files.

---
+++ Butchers/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Butchers.Startup))]
+namespace Butchers
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
