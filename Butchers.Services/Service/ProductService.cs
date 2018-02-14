29995c3e51211e241c4efddc97ae15fb6c2916d9
dangreenacres <dangreenacres@gmail.com>
2018-02-14 09:47:07 +0000

    Updated Database & EDMX.
    Added necessary classes & interfaces to Data and Service folders.

---
+++ Butchers.Services/Service/ProductService.cs
@@ -0,0 +1,13 @@
+﻿using Butchers.Services.IService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Butchers.Services.Service
+{
+    public class ProductService : IProductService
+    {
+    }
+}
