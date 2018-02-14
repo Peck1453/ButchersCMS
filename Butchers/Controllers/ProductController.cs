e6afb4fe548b72b3eeaff7000a314ad12e5073c9
dangreenacres <dangreenacres@gmail.com>
2018-02-14 12:13:39 +0000

    Product controller and functionality for listing meats has been implemented

---
+++ Butchers/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using Butchers.Services.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Butchers.Controllers
+{
+    public class ProductController : ApplicationController
+    {
+        public ProductController()
+        {
+            //_productService = new ProductService();
+        }
+
+        // GET: Meat
+        public ActionResult Meats()
+        {
+            return View(_productService.GetMeats());
+        }
+    }
+}
