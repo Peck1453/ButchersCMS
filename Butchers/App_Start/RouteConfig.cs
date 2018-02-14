173f04992fdd1ce172c1a1acf482db3b3fda105b
b7034288 <b7034288@AC03-S23E202887>
2018-02-14 20:22:58 +0000

    Created View and Implemented displaying Promo Codes

--- Butchers/App_Start/RouteConfig.cs
+++ Butchers/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Butchers
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Order", action = "GetPromoCodes", id = UrlParameter.Optional }
             );
         }
     }
