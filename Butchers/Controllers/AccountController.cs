f87b337e78c29ba89c8d44b636043458a40f672b
dangreenacres <dangreenacres@gmail.com>
2018-03-07 13:09:42 +0000

    Implemented authorisation functionality

--- Butchers/Controllers/AccountController.cs
+++ Butchers/Controllers/AccountController.cs
@@ -156,7 +156,10 @@ namespace Butchers.Controllers
                 if (result.Succeeded)
                 {
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
-                    
+
+                    // Automatically adds a user to the role Customer
+                    UserManager.AddToRole(user.Id, "Customer");
+
                     // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
                     // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
